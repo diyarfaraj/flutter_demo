f5ba9e8e8b91bb26a104269ea4895557534164c0
diyarfaraj <diyar.faraj@gmail.com>
2020-11-01 13:38:24 +0100

    question widget added

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo/question.dart';
+import './main.dart';
 
 void main() {
   runApp(DemoApp());
 }
 
-class DemoApp extends StatelessWidget {
+class DemoApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _DemoAppState();
+  }
+}
+
+class _DemoAppState extends State<DemoApp> {
+  var _questionIndex = 0;
+
   myfunction() {
-    print('heheheh');
+    setState(() {
+      _questionIndex++;
+    });
+    print(_questionIndex);
   }
 
+  @override
   Widget build(BuildContext context) {
+    var questions = ['Din favorit färg?', 'Din favorit djur?'];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -17,7 +34,9 @@ class DemoApp extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-            Text('hejhej'),
+            Question(
+              questions[_questionIndex],
+            ),
             RaisedButton(
               child: Text('answer 1'),
               onPressed: myfunction,
